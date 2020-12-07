f516abf6dad38a8840d21ccbf99536f471bcaac6
smichhal <70816854+smichhal@users.noreply.github.com>
2020-12-06 18:42:15 -0800

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,26 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visiblility = 255
+
+  }
+
+  display(){
+    console.log(this.body.speed);
+
+    if(this.body.speed < 3){
+    super.display();
+    }
+    else{
+      World.remove(world, this.body);
+      push();
+      tint(255, this.visiblility);
+      this.visiblility = this.visiblility - 5
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      pop();
+      
+    }
+  }
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# VanishingPigs
+# AngryBirdsStage4
